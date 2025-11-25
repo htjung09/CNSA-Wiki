126349e5ce0abb6c78ee7f6753a8cc542ff4f48c
htjung09 <96168935+htjung09@users.noreply.github.com>
2025-11-25 15:19:29 +0900

    LoginPage

--- WikiApi/Controllers/AuthController.cs
+++ WikiApi/Controllers/AuthController.cs
@@ -2,6 +2,7 @@
 using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata;
 using SharedData.DTOs;
 using WikiApi.Data;
 using WikiApi.Services;
